00566988176d2ea9cb3357cb24476098ef595829
robslawinski <rslawinski@verizon.net>
2018-04-21 21:07:15 -0400

    Implementation of Mobile UI (Modals)
    Attempted to use navigation controllers. Macbook and Android Emulator
    was required. Significant waste of time.

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import PreviewScreen from './src/components/PreviewScreen';
 
 export default class App extends React.Component {
   render() {
     return (
+
       <View style={styles.container}>
+      <PreviewScreen/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -13,6 +17,13 @@ export default class App extends React.Component {
   }
 }
 
+
+/*
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,3 +32,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/*
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+*/
+
+
+
---
+++ src/components/PreviewScreen.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {Modal, Text, TouchableHighlight, View, Button} from 'react-native';
+
+class PreviewScreen extends Component {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  componentWillReceiveProps( newProps ){
+      this.setState( {modalVisible : newProps.visible});
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Button title = "Test"
+           onPress={() => {
+             this.setModalVisible(!this.state.modalVisible);
+            }}
+        />
+
+ 
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+
+              <Text>Hello World!</Text>
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+export default PreviewScreen;
